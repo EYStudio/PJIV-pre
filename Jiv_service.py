a9be62d5e3824ef84bb6eafbabe3a92301c8ddec
Errorsia <Errorsia@outlook.com>
2025-12-02 21:30:27 +0800

    rename Jiv_worker.py to Jiv_service.py

--- Jiv_service.py
+++ Jiv_service.py
@@ -3,37 +3,37 @@ import time
 from abc import ABC, abstractmethod
 
 
-class WorkerManager:
+class ServiceManager:
     def __init__(self, logic, gui):
         super().__init__()
         self.logic = logic
         self.gui = gui
-        self.workers = []
+        self.services = []
 
         self.threads = {}
 
-        self.init_workers()
+        self.init_services()
         self.start_all()
 
-    def init_workers(self):
-        self.workers.append(TopMostWorker(50, self.logic, self.gui))
+    def init_services(self):
+        self.services.append(TopMostService(50, self.logic, self.gui))
 
     def start_all(self):
-        for worker in self.workers:
-            thread = threading.Thread(target=worker.start, daemon=True)
-            self.threads[worker] = thread
+        for service in self.services:
+            thread = threading.Thread(target=service.start, daemon=True)
+            self.threads[service] = thread
             thread.start()
 
     def stop_all(self):
-        """停止所有 worker 并安全退出线程"""
-        for worker, thread in self.threads.items():
-            worker.stop()
-            # 如果不是 daemon，可以 join 等待线程结束
+        """Stop services and quit"""
+        for service, thread in self.threads.items():
+            service.stop()
+            # If not daemon, wait until threads end
             if thread.is_alive():
                 thread.join()
 
 
-class BaseWorkerInterface(ABC):
+class BaseServiceInterface(ABC):
     @abstractmethod
     def start(self):
         raise NotImplementedError("Subclasses must implement start()")
@@ -47,7 +47,7 @@ class BaseWorkerInterface(ABC):
         raise NotImplementedError("Subclasses must implement run_task()")
 
 
-class TopMostWorker(BaseWorkerInterface):
+class TopMostService(BaseServiceInterface):
     def __init__(self, interval, logic, gui):
         """
         :param interval: millisecond
